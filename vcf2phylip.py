4155a5170bf2c400e3d9e9439bb57773a552b62f
edgardomortiz <e.ortiz.v@gmail.com>
2017-10-17 19:55:27 -0500

    Minsample bug fixed, option parser added

--- vcf2phylip.py
+++ vcf2phylip.py
@@ -14,188 +14,199 @@ phylogenetic analysis. The code is optmizied to process VCF files with sizes
 
 __author__      = "Edgardo M. Ortiz"
 __credits__     = "Juan D. Palacio-Mejía"
-__version__     = "1.1"
+__version__     = "1.2"
 __email__       = "e.ortiz.v@gmail.com"
 __date__        = "2017-10-10"
 
 
-
 import sys
 import os
-
-# First argument is the name of the VCF file
-filename = sys.argv[1]
-
-# Second argument is minimum of samples per SNP, default is 4 for phylogenetics
-try: min_sample_locus = int(sys.argv[2])
-except (ValueError, IndexError): min_sample_locus = 4
-
-# Third optional argument is name of outgroup sample
-try: outgroup = sys.argv[3]
-except (ValueError, IndexError): outgroup = ""
-
-# Output filename will be the same as input file, indicating the minimum of samples specified
-# e.g. VCF: sample.vcf, PHY: sample_min4.phy
-outfile = filename.split(".")[0]+"_min"+str(min_sample_locus)+"."+filename.split(".")[-1].replace("vcf","phy").replace("VCF","phy")
-
-# Dictionary of IUPAC ambiguities for nucleotides
-amb = {("A","A"):"A",
-	   ("A","C"):"M",
-	   ("A","G"):"R",
-	   ("A","N"):"A",
-	   ("A","T"):"W",
-	   ("C","A"):"M",
-	   ("C","C"):"C",
-	   ("C","G"):"S",
-	   ("C","N"):"C",
-	   ("C","T"):"Y",
-	   ("G","A"):"R",
-	   ("G","C"):"S",
-	   ("G","G"):"G",
-	   ("G","N"):"G",
-	   ("G","T"):"K",
-	   ("N","A"):"A",
-	   ("N","C"):"C",
-	   ("N","G"):"G",
-	   ("N","N"):"N",
-	   ("N","T"):"T",
-	   ("T","A"):"W",
-	   ("T","C"):"Y",
-	   ("T","G"):"K",
-	   ("T","N"):"T",
-	   ("T","T"):"T"}
+import argparse
 
 
+def main():
+	parser = argparse.ArgumentParser(description="Coverts SNPs in VCF format into a PHYLIP matrix for phylogenetic analysis")
+	parser.add_argument("-i", "--input", action="store", dest="filename", required=True,
+		help="Name of the input VCF file")
+	parser.add_argument("-s", "--min-samples-locus", action="store", dest="min_samples_locus", type=int, default=4,
+		help="Minimum of samples required to be present at a locus, default=4 since is the minimum for phylogenetics")
+	parser.add_argument("-o", "--outgroup", action="store", dest="outgroup", default="",
+		help="Name of the outgroup in the matrix. Sequence will be written as first taxon in the alignment since most phylogenetic software roots the tree at the first taxon in alignment.")
+	args = parser.parse_args()
 
-# Process header of VCF file
-with open(filename) as vcf:
+	filename = args.filename
+	min_samples_locus = args.min_samples_locus
+	outgroup = args.outgroup
 
-	# List to store sample names
-	sample_names = []
+	# Output filename will be the same as input file, indicating the minimum of samples specified
+	# e.g. VCF: sample.vcf, PHY: sample_min4.phy
+	outfile = filename.split(".")[0]+"_min"+str(min_samples_locus)+"."+filename.split(".")[-1].replace("vcf","phy").replace("VCF","phy")
 
-	# Keep track of longest sequence name for padding with spaces in the output file
-	len_longest_name = 0
-
-	# Look for the line in the VCF header with the sample names
-	for line in vcf:
-		if line.startswith("#CHROM"):
-
-			# Split line into fields
-			broken = line.strip("\n").split("\t")
-
-			# Create a list of sample names and the length of the longest name
-			for i in range(9, len(broken)):
-				sample_names.append(broken[i])
-				len_longest_name = max(len_longest_name, len(broken[i]))
-			break
-vcf.close()
+	# Dictionary of IUPAC ambiguities for nucleotides
+	amb = {("A","A"):"A",
+		   ("A","C"):"M",
+		   ("A","G"):"R",
+		   ("A","N"):"A",
+		   ("A","T"):"W",
+		   ("C","A"):"M",
+		   ("C","C"):"C",
+		   ("C","G"):"S",
+		   ("C","N"):"C",
+		   ("C","T"):"Y",
+		   ("G","A"):"R",
+		   ("G","C"):"S",
+		   ("G","G"):"G",
+		   ("G","N"):"G",
+		   ("G","T"):"K",
+		   ("N","A"):"A",
+		   ("N","C"):"C",
+		   ("N","G"):"G",
+		   ("N","N"):"N",
+		   ("N","T"):"T",
+		   ("T","A"):"W",
+		   ("T","C"):"Y",
+		   ("T","G"):"K",
+		   ("T","N"):"T",
+		   ("T","T"):"T"}
 
 
 
-# We need to create an intermediate file to hold the sequence data vertically
-# and then transpose it to create the PHYLIP file
-temporal = open(outfile+".tmp", "w")
+	# Process header of VCF file
+	with open(filename) as vcf:
 
-# Store the index of the last column of VCF file
-index_last_sample = len(sample_names)+9
+		# List to store sample names
+		sample_names = []
 
-# Start processing SNPs of VCF file
-with open(filename) as vcf:
+		# Keep track of longest sequence name for padding with spaces in the output file
+		len_longest_name = 0
 
-	# Initialize line counter
-	snp_num = 0
-	while True:
+		# Look for the line in the VCF header with the sample names
+		for line in vcf:
+			if line.startswith("#CHROM"):
 
-		# Load large chunks of file into memory
-		vcf_chunk = vcf.readlines(100000)
-		if not vcf_chunk:
-			break
-
-		# Now process the SNPs one by one
-		for line in vcf_chunk:
-			if not line.startswith("#"):
-
-				# Split line into columns
+				# Split line into fields
 				broken = line.strip("\n").split("\t")
 
-				# Print progress every 100,000 lines
-				snp_num += 1
-				if snp_num % 100000 == 0:
-					print str(snp_num)+" SNPs processed"
-
-				# If SNP meets minimum of samples requirement
-				if int(broken[7].split(";")[0].replace("NS=","")) >= min_sample_locus:
-
-					# Create a dictionary for genotype to nucleotide translation
-					# each SNP may code the nucleotides in a different manner
-					nuc = {str(0):broken[3], ".":"N"}
-					for n in range(len(broken[4].split(","))):
-						nuc[str(n+1)] = broken[4].split(",")[n]
-
-					# Translate genotypes into nucleotides and the obtain the IUPAC ambiguity
-					# for heterozygous SNPs, and append to DNA sequence of each sample
-					site_tmp = ''.join([(amb[(nuc[broken[i][0]], nuc[broken[i][2]])]) for i in range(9, index_last_sample)])
-
-					# Write entire row of single nucleotide genotypes to temporary file
-					temporal.write(site_tmp+"\n")
-vcf.close()
-temporal.close()
+				# Create a list of sample names and the length of the longest name
+				for i in range(9, len(broken)):
+					sample_names.append(broken[i])
+					len_longest_name = max(len_longest_name, len(broken[i]))
+				break
+	vcf.close()
 
 
 
-# Write PHYLIP file
-output = open(outfile, "w")
+	# We need to create an intermediate file to hold the sequence data vertically
+	# and then transpose it to create the PHYLIP file
+	temporal = open(outfile+".tmp", "w")
 
-# PHYLIP header is number of samples and number of sites in alignment
-header = str(len(sample_names))+" "+str(snp_num)+"\n"
-output.write(header)
+	# Store the index of the last column of VCF file
+	index_last_sample = len(sample_names)+9
 
-# Index of outgroup in list of sample names
-idx_outgroup = "NA"
+	# Start processing SNPs of VCF file
+	with open(filename) as vcf:
 
-# Write outgroup as first sequence in alignment if the name is specified
-if outgroup in sample_names:
-	idx_outgroup = sample_names.index(outgroup)
-	with open(outfile+".tmp") as tmp_seq:
-		seqout = ""
-		for line in tmp_seq:
-			seqout += line[idx_outgroup]
+		# Initialize line counter
+		snp_num = 0
+		snp_filtered = 0
+		while True:
 
-		# Add padding with spaces after sequence name so every nucleotide starts
-		# at the same position (only aestethic)
-		padding = (len_longest_name + 3 - len(sample_names[idx_outgroup])) * " "
+			# Load large chunks of file into memory
+			vcf_chunk = vcf.readlines(100000)
+			if not vcf_chunk:
+				break
 
-		# Write sample name and its corresponding sequence
-		output.write(sample_names[idx_outgroup]+padding+seqout+"\n")
+			# Now process the SNPs one by one
+			for line in vcf_chunk:
+				if not line.startswith("#"):
 
-		# Print current progress
-		print "Outgroup "+outgroup+" added to PHYLIP file."
+					# Split line into columns
+					broken = line.strip("\n").split("\t")
 
-# Write sequences of the ingroup
-for s in range(0, len(sample_names)):
-	if s != idx_outgroup:
+					# Print progress every 100,000 lines
+					snp_num += 1
+					if snp_num % 100000 == 0:
+						print str(snp_num)+" SNPs processed"
+
+					# If SNP meets minimum of samples requirement
+					if int(broken[7].split(";")[0].replace("NS=","")) >= min_samples_locus:
+
+						# Add to running sum of filtered SNPs
+						snp_filtered += 1
+
+						# Create a dictionary for genotype to nucleotide translation
+						# each SNP may code the nucleotides in a different manner
+						nuc = {str(0):broken[3], ".":"N"}
+						for n in range(len(broken[4].split(","))):
+							nuc[str(n+1)] = broken[4].split(",")[n]
+
+						# Translate genotypes into nucleotides and the obtain the IUPAC ambiguity
+						# for heterozygous SNPs, and append to DNA sequence of each sample
+						site_tmp = ''.join([(amb[(nuc[broken[i][0]], nuc[broken[i][2]])]) for i in range(9, index_last_sample)])
+
+						# Write entire row of single nucleotide genotypes to temporary file
+						temporal.write(site_tmp+"\n")
+	vcf.close()
+	temporal.close()
+
+
+
+	# Write PHYLIP file
+	output = open(outfile, "w")
+
+	# PHYLIP header is number of samples and number of sites in alignment
+	header = str(len(sample_names))+" "+str(snp_filtered)+"\n"
+	output.write(header)
+
+	# Index of outgroup in list of sample names
+	idx_outgroup = "NA"
+
+	# Write outgroup as first sequence in alignment if the name is specified
+	if outgroup in sample_names:
+		idx_outgroup = sample_names.index(outgroup)
 		with open(outfile+".tmp") as tmp_seq:
 			seqout = ""
 			for line in tmp_seq:
-				seqout += line[s]
+				seqout += line[idx_outgroup]
 
 			# Add padding with spaces after sequence name so every nucleotide starts
 			# at the same position (only aestethic)
-			padding = (len_longest_name + 3 - len(sample_names[s])) * " "
+			padding = (len_longest_name + 3 - len(sample_names[idx_outgroup])) * " "
 
 			# Write sample name and its corresponding sequence
-			output.write(sample_names[s]+padding+seqout+"\n")
+			output.write(sample_names[idx_outgroup]+padding+seqout+"\n")
 
 			# Print current progress
-			print "Sample "+str(s+1)+" of "+str(len(sample_names))+" added to PHYLIP file."
+			print "Outgroup "+outgroup+" added to PHYLIP file."
 
-output.close()
+	# Write sequences of the ingroup
+	for s in range(0, len(sample_names)):
+		if s != idx_outgroup:
+			with open(outfile+".tmp") as tmp_seq:
+				seqout = ""
+				for line in tmp_seq:
+					seqout += line[s]
+
+				# Add padding with spaces after sequence name so every nucleotide starts
+				# at the same position (only aestethic)
+				padding = (len_longest_name + 3 - len(sample_names[s])) * " "
+
+				# Write sample name and its corresponding sequence
+				output.write(sample_names[s]+padding+seqout+"\n")
+
+				# Print current progress
+				print "Sample "+str(s+1)+" of "+str(len(sample_names))+" added to PHYLIP file."
+
+	output.close()
 
 
-# Remove temporary file
-os.remove(outfile+".tmp")
+	# Remove temporary file
+	os.remove(outfile+".tmp")
 
 
-print "Done!\n"
+	print "Done!\n"
 
-# END
+
+if __name__ == "__main__":
+    main()
+
