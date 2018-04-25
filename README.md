bc838d82a74b6b59e6bc6fd43e5b37b4ab4720f8
Edgardo Ortiz <e.ortiz.v@gmail.com>
2018-04-25 12:00:08 +0200

    Release of v.1.4
    The code has been revised, and new options added. Simultaneous creation of PHYLIP, NEXUS, binary NEXUS, and FASTA matrices is now possible.

--- README.md
+++ README.md
@@ -2,49 +2,72 @@
 Convert SNPs in VCF format to PHYLIP for phylogenetic analysis
 
 ## _Brief description_
-The script converts a collection of SNPs in VCF format into a PHYLIP file for phylogenetic analysis. In case of heterozygous SNPs the IUPAC ambiguity code is used to construct the final DNA sequence. The code is optimized for large VCF files, in our tests it processed a 20GB VCF (~3 million SNPs x 650 individuals) in ~27 minutes. Once the PHYLIP file is obtained, conversion to other alignment formats (e.g. FASTA) is trivial with other software like [_Aliview_](http://ormbunkar.se/aliview/).
+This script takes as input a VCF file and will use the SNP genotypes to create a matrix for phylogenetic analysis in the PHYLIP, FASTA, NEXUS, or binary NEXUS formats. For heterozygous SNPs the consensus is made and the IUPAC nucleotide ambiguity codes are written to the final matrix(ces). The code is optimized for large VCF matrices (hundreds of samples and millions of genotypes), for example, in our tests it processed a 20GB VCF (~3 million SNPs x 650 individuals) in ~27 minutes. The initial version of the script just produced a PHYLIP matrix but now we have added other popular formats, including the binary NEXUS file to run SNPs analysis with the SNAPP plugin in BEAST.
 
-A minimum number of samples per SNP can also be specified so you won't need to run again the software that created your VCF file just to increase this threshold. Lastly, if you specify an OUTGROUP the sequence of that sample will be written first in the alignment (phylogenetic software usually root the trees on the first taxon in the alignment).
+Additionally, you can choose a minimum number of samples per SNP to control the final amount of missing data. Since phylogenetic software usually root the trees at the first sequence in the alignment (e.g. RAxML, IQTREE, and MrBayes), the script also allows you to specify an OUTGROUP sequence that will be written in the first place in the alignment.
 
-The script has been tested with VCF files produced by [_pyrad v.3.0.66_](https://github.com/dereneaton/pyrad), [_ipyrad v.0.7.x_](http://ipyrad.readthedocs.io/), and [_Stacks v.1.47_](http://catchenlab.life.illinois.edu/stacks/).
+The script has been tested with VCF files produced by [*pyrad v.3.0.66*](https://github.com/dereneaton/pyrad), [*ipyrad v.0.7.x*](http://ipyrad.readthedocs.io/), [*Stacks v.1.47*](http://catchenlab.life.illinois.edu/stacks/), [*dDocent*](http://ddocent.com/), and [*GATK*](https://software.broadinstitute.org/gatk/).
+
+Please don't hesitate to open an "Issue" if you find any problem.
 
 ## _Usage_
 Just type `python vcf2phylip.py -h` to show the help of the program:
 
 ```
-usage: vcf2phylip.py [-h] -i FILENAME [-s MIN_SAMPLES_LOCUS] [-o OUTGROUP]
+usage: vcf2phylip.py [-h] -i FILENAME [-m MIN_SAMPLES_LOCUS] [-o OUTGROUP]
+                     [-p] [-f] [-n] [-b]
 
-Converts SNPs in VCF format into a PHYLIP matrix for phylogenetic analysis
+Converts SNPs in VCF format into an alignment for phylogenetic analysis
 
 optional arguments:
   -h, --help            show this help message and exit
   -i FILENAME, --input FILENAME
                         Name of the input VCF file
-  -s MIN_SAMPLES_LOCUS, --min-samples-locus MIN_SAMPLES_LOCUS
+  -m MIN_SAMPLES_LOCUS, --min-samples-locus MIN_SAMPLES_LOCUS
                         Minimum of samples required to be present at a locus,
-                        default=4 since is the minimum for phylogenetics
+                        default=4 since is the minimum for phylogenetics.
   -o OUTGROUP, --outgroup OUTGROUP
                         Name of the outgroup in the matrix. Sequence will be
-                        written as first taxon in the alignment
+                        written as first taxon in the alignment.
+  -p, --phylip-disable  A PHYLIP matrix is written by default unless you
+                        enable this flag
+  -f, --fasta           Write a FASTA matrix, disabled by default
+  -n, --nexus           Write a NEXUS matrix, disabled by default
+  -b, --nexus-binary    Write a binary NEXUS matrix for analysis of biallelic
+                        SNPs in SNAPP, disabled by default
 ```
 
 ## _Examples_
-_Example 1:_ Using the default minimum of samples per SNP of 4:
+_Example 1:_ Use default parameters to create a PHYLIP matrix with a minimum of 4 samples per SNP:
 ```bash
+python vcf2phylip.py --input myfile.vcf
+# Which i equivalent to:
 python vcf2phylip.py -i myfile.vcf
 # This command will create a PHYLIP called myfile_min4.phy
 ```
 
-_Example 2:_ Using a custom minimum of samples per SNP of 63:
+_Example 2:_ Create a PHYLIP and a FASTA matrix using a minimum of 60 samples per SNP:
 ```bash
-python vcf2phylip.py -i myfile.vcf -s 63
-# This command will create a PHYLIP called myfile_min63.phy
+python vcf2phylip.py --input myfile.vcf --fasta --min-samples-locus 60
+# Which is equivalent to:
+python vcf2phylip.py -i myfile.vcf -f -m 60
+# This command will create a PHYLIP called myfile_min60.phy and a FASTA called myfile_min60.fasta
 ```
 
-_Example 3:_ Using a custom minimum of samples per SNP of 35 and setting outgroup to "sample1":
+_Example 3:_ Create all output formats, and select "sample1" as outgroup:
 ```bash
-python vcf2phylip.py -i myfile.vcf -s 35 -o sample1
-# This command will create a PHYLIP called myfile_min35.phy with sample1 as the first sequence
+python vcf2phylip.py --input myfile.vcf --outgroup sample1 --fasta --nexus --nexus-binary
+# Which is equivalent to:
+python vcf2phylip.py -i myfile.vcf -o sample1 -f -n -b
+# This command will create a PHYLIP called myfile_min4.phy, a FASTA called myfile_min4.fasta, a NEXUS called myfile_min4.nexus, and a binary NEXUS called myfile_min4.bin.nexus
+```
+
+_Example 4:_ If, for example, you wish to disable the creation of the PHYLIP matrix and only create a NEXUS matrix:
+```bash
+python vcf2phylip.py --input myfile.vcf --phylip-disable --nexus
+# Which is equivalent to:
+python vcf2phylip.py -i myfile.vcf -p -n
+# This command will create only a NEXUS matrix called myfile_min4.nexus
 ```
 
 ## _Credits_
--- vcf2phylip.py
+++ vcf2phylip.py
@@ -4,19 +4,18 @@
 
 
 '''
-The script converts a collection of SNPs in VCF format into a PHYLIP file for
-phylogenetic analysis. The code is optmizied to process VCF files with sizes 
->1GB. For small VCF files the algorithm slows down as the number of taxa increases
-(but is still fast).
+The script converts a collection of SNPs in VCF format into a PHYLIP, FASTA, 
+NEXUS, or binary NEXUS file for phylogenetic analysis. The code is optimized
+to process VCF files with sizes >1GB. For small VCF files the algorithm slows
+down as the number of taxa increases (but is still fast).
 '''
-
 
 
 __author__      = "Edgardo M. Ortiz"
 __credits__     = "Juan D. Palacio-Mejía"
-__version__     = "1.3"
+__version__     = "1.4"
 __email__       = "e.ortiz.v@gmail.com"
-__date__        = "2017-10-10"
+__date__        = "2018-04-24"
 
 
 import sys
@@ -25,24 +24,36 @@ import argparse
 
 
 def main():
-	parser = argparse.ArgumentParser(description="Converts SNPs in VCF format into a PHYLIP matrix for phylogenetic analysis")
+	parser = argparse.ArgumentParser(description="Converts SNPs in VCF format into an alignment for phylogenetic analysis")
 	parser.add_argument("-i", "--input", action="store", dest="filename", required=True,
 		help="Name of the input VCF file")
-	parser.add_argument("-s", "--min-samples-locus", action="store", dest="min_samples_locus", type=int, default=4,
-		help="Minimum of samples required to be present at a locus, default=4 since is the minimum for phylogenetics")
+	parser.add_argument("-m", "--min-samples-locus", action="store", dest="min_samples_locus", type=int, default=4,
+		help="Minimum of samples required to be present at a locus, default=4 since is the minimum for phylogenetics.")
 	parser.add_argument("-o", "--outgroup", action="store", dest="outgroup", default="",
-		help="Name of the outgroup in the matrix. Sequence will be written as first taxon in the alignment")
+		help="Name of the outgroup in the matrix. Sequence will be written as first taxon in the alignment.")
+	parser.add_argument("-p", "--phylip-disable", action="store_true", dest="phylipdisable", default=False,
+		help="A PHYLIP matrix is written by default unless you enable this flag")
+	parser.add_argument("-f", "--fasta", action="store_true", dest="fasta", default=False,
+		help="Write a FASTA matrix, disabled by default")
+	parser.add_argument("-n", "--nexus", action="store_true", dest="nexus", default=False,
+		help="Write a NEXUS matrix, disabled by default")
+	parser.add_argument("-b", "--nexus-binary", action="store_true", dest="nexusbin", default=False,
+		help="Write a binary NEXUS matrix for analysis of biallelic SNPs in SNAPP, disabled by default")
 	args = parser.parse_args()
+
 
 	filename = args.filename
 	min_samples_locus = args.min_samples_locus
 	outgroup = args.outgroup
+	phylipdisable = args.phylipdisable
+	fasta = args.fasta
+	nexus = args.nexus
+	nexusbin = args.nexusbin
 
-	# Output filename will be the same as input file, indicating the minimum of samples specified
-	# e.g. VCF: sample.vcf, PHY: sample_min4.phy
-	outfile = filename.replace(".vcf",".min"+str(min_samples_locus)+".phy")
 
 	# Dictionary of IUPAC ambiguities for nucleotides
+	# '*' means deletion for GATK (and other software?)
+	# Deletions are ignored when making the consensus
 	amb = {("A","A"):"A",
 		   ("A","C"):"M",
 		   ("A","G"):"R",
@@ -67,14 +78,40 @@ def main():
 		   ("T","C"):"Y",
 		   ("T","G"):"K",
 		   ("T","N"):"T",
-		   ("T","T"):"T"}
+		   ("T","T"):"T",
+		   ("*","*"):"-",
+		   ("A","*"):"A",
+		   ("*","A"):"A",
+		   ("C","*"):"C",
+		   ("*","C"):"C",
+		   ("G","*"):"G",
+		   ("*","G"):"G",
+		   ("T","*"):"T",
+		   ("*","T"):"T",
+		   ("N","*"):"N",
+		   ("*","N"):"N"}
 
+
+	# Dictionary for translating biallelic SNPs into SNAPP
+	# 0 is homozygous reference
+	# 1 is heterozygous
+	# 2 is homozygous alternative
+	gen_bin = {"./.":"?",
+			   ".|.":"?",
+			   "0/0":"0",
+			   "0|0":"0",
+			   "0/1":"1",
+			   "0|1":"1",
+			   "1/0":"1",
+			   "1|0":"1",
+			   "1/1":"2",
+			   "1/1":"2"}
 
 
 	# Process header of VCF file
 	with open(filename) as vcf:
 
-		# List to store sample names
+		# Create a list to store sample names
 		sample_names = []
 
 		# Keep track of longest sequence name for padding with spaces in the output file
@@ -87,20 +124,36 @@ def main():
 				# Split line into fields
 				broken = line.strip("\n").split("\t")
 
-				# Create a list of sample names and the length of the longest name
+				# If the minimum-samples-per-locus parameter is larger than the number of
+				# species in the alignment make it the same as the number of species
+				if min_samples_locus > len(broken[9:]):
+					min_samples_locus = len(broken[9:])
+
+				# Create a list of sample names and the keep track of the longest name length
 				for i in range(9, len(broken)):
-					sample_names.append(broken[i])
-					len_longest_name = max(len_longest_name, len(broken[i]))
+					name_sample = broken[i].replace("./","") # GATK adds "./" to sample names
+					sample_names.append(name_sample)
+					len_longest_name = max(len_longest_name, len(name_sample))
 				break
+
 	vcf.close()
 
 
+	# Output filename will be the same as input file, indicating the minimum of samples specified
+	outfile = filename.replace(".vcf",".min"+str(min_samples_locus))
 
-	# We need to create an intermediate file to hold the sequence data vertically
-	# and then transpose it to create the PHYLIP file
-	temporal = open(outfile+".tmp", "w")
+	# We need to create an intermediate file to hold the sequence data 
+	# vertically and then transpose it to create the matrices
+	if fasta or nexus or not phylipdisable:
+		temporal = open(outfile+".tmp", "w")
+	
+	# if binary NEXUS is selected also create a separate temporal
+	if nexusbin:
+		temporalbin = open(outfile+".bin.tmp", "w")
 
-	# Store the index of the last column of VCF file
+
+	##################
+	# PROCESS VCF FILE
 	index_last_sample = len(sample_names)+9
 
 	# Start processing SNPs of VCF file
@@ -108,7 +161,10 @@ def main():
 
 		# Initialize line counter
 		snp_num = 0
-		snp_filtered = 0
+		snp_accepted = 0
+		snp_shallow = 0
+		snp_multinuc = 0
+		snp_biallelic = 0
 		while True:
 
 			# Load large chunks of file into memory
@@ -123,109 +179,205 @@ def main():
 					# Split line into columns
 					broken = line.strip("\n").split("\t")
 
-					# Print progress every 100,000 lines
+					# Keep track of number of genotypes processed
 					snp_num += 1
-					if snp_num % 100000 == 0:
-						print str(snp_num)+" SNPs processed"
 
-					# If SNP meets minimum of samples requirement
-					if "NS=" in broken[7]: # for stacks, pyrad, ipyrad NS means Number of Samples in locus
-						if int(broken[7].split("NS=")[1].split(";")[0]) >= min_samples_locus:
+					# Print progress every 500000 lines
+					if snp_num % 500000 == 0:
+						print str(snp_num)+" genotypes processed"
 
-							# Add to running sum of filtered SNPs
-							snp_filtered += 1
+					# Check if the SNP has the minimum of samples required
+					if (len(broken[9:]) - ''.join(broken[9:]).count("./.")) >= min_samples_locus:
+						
+						# Check that ref genotype is a single nucleotide and alternative genotypes are single nucleotides
+						if len(broken[3]) == 1 and (len(broken[4])-broken[4].count(",")) == (broken[4].count(",")+1):
 
-							# Create a dictionary for genotype to nucleotide translation
-							# each SNP may code the nucleotides in a different manner
-							nuc = {str(0):broken[3], ".":"N"}
-							for n in range(len(broken[4].split(","))):
-								nuc[str(n+1)] = broken[4].split(",")[n]
+							# Add to running sum of accepted SNPs
+							snp_accepted += 1
 
-							# Translate genotypes into nucleotides and the obtain the IUPAC ambiguity
-							# for heterozygous SNPs, and append to DNA sequence of each sample
-							site_tmp = ''.join([(amb[(nuc[broken[i][0]], nuc[broken[i][2]])]) for i in range(9, index_last_sample)])
+							# If nucleotide matrices are requested
+							if fasta or nexus or not phylipdisable:
 
-							# Write entire row of single nucleotide genotypes to temporary file
-							temporal.write(site_tmp+"\n")
+								# Create a dictionary for genotype to nucleotide translation
+								# each SNP may code the nucleotides in a different manner
+								nuc = {str(0):broken[3], ".":"N"}
+								for n in range(len(broken[4].split(","))):
+									nuc[str(n+1)] = broken[4].split(",")[n]
 
-					# Optimize by reducing repetition of code when possible, otherwise seems to work fine
-					elif "WT=" in broken[7] and "HET=" in broken[7] and "HOM=" in broken[7] and "NC=" in broken[7]: # Regina's format from samtools
-						if len(sample_names) - int(broken[7].split(";")[4].replace("NC=","")) >= min_samples_locus:
+								# Translate genotypes into nucleotides and the obtain the IUPAC ambiguity
+								# for heterozygous SNPs, and append to DNA sequence of each sample
+								site_tmp = ''.join([(amb[(nuc[broken[i][0]], nuc[broken[i][2]])]) for i in range(9, index_last_sample)])
 
-							# Add to running sum of filtered SNPs
-							snp_filtered += 1
+								# Write entire row of single nucleotide genotypes to temporary file
+								temporal.write(site_tmp+"\n")
 
-							# Create a dictionary for genotype to nucleotide translation
-							# each SNP may code the nucleotides in a different manner
-							nuc = {str(0):broken[3], ".":"N"}
-							for n in range(len(broken[4].split(","))):
-								nuc[str(n+1)] = broken[4].split(",")[n]
+							# Write binary NEXUS for SNAPP if requested
+							if nexusbin:
 
-							# Translate genotypes into nucleotides and the obtain the IUPAC ambiguity
-							# for heterozygous SNPs, and append to DNA sequence of each sample
-							site_tmp = ''.join([(amb[(nuc[broken[i][0]], nuc[broken[i][2]])]) for i in range(9, index_last_sample)])
+								# Check taht the SNP only has two alleles
+								if len(broken[4]) == 1:
+									
+									# Add to running sum of biallelic SNPs
+									snp_biallelic += 1
 
-							# Write entire row of single nucleotide genotypes to temporary file
-							temporal.write(site_tmp+"\n")
+									# Translate genotype into 0 for homozygous ref, 1 for heterozygous, and 2 for homozygous alt
+									binsite_tmp = ''.join([(gen_bin[broken[i][0:3]]) for i in range(9, index_last_sample)])
+
+									# Write entire row to temporary file
+									temporalbin.write(binsite_tmp+"\n")
+
+						else:
+							# Keep track of loci rejected due to multinucleotide genotypes
+							snp_multinuc += 1
+							# Keep track of loci rejected due to exceeded missing data
+							snp_shallow += 1
+
+					else:
+						# Keep track of loci rejected due to exceeded missing data
+						snp_shallow += 1
+
+		# Print useful information about filtering of SNPs
+		print str(snp_num) + " genotypes processed in total"
+		print "\n"
+		print str(snp_shallow) + " genotypes were excluded because they exceeded the amount of missing data allowed"
+		print str(snp_multinuc) + " genotypes passed missing data filter but were excluded for not being SNPs"
+		print str(snp_accepted) + " SNPs passed the filters"
+		if nexusbin:
+			print str(snp_biallelic) + " SNPs were biallelic and selected for binary NEXUS"
+		print "\n"
+
 	vcf.close()
-	temporal.close()
+	if fasta or nexus or not phylipdisable:
+		temporal.close()
+	if nexusbin:
+		temporalbin.close()
 
 
+	#######################
+	# WRITE OUTPUT MATRICES
 
-	# Write PHYLIP file
-	output = open(outfile, "w")
+	if not phylipdisable:
+		output_phy = open(outfile+".phy", "w")
+		header_phy = str(len(sample_names))+" "+str(snp_accepted)+"\n"
+		output_phy.write(header_phy)
 
-	# PHYLIP header is number of samples and number of sites in alignment
-	header = str(len(sample_names))+" "+str(snp_filtered)+"\n"
-	output.write(header)
+	if fasta:
+		output_fas = open(outfile+".fasta", "w")
 
-	# Index of outgroup in list of sample names
+	if nexus:
+		output_nex = open(outfile+".nexus", "w")
+		header_nex = "#NEXUS\n\nBEGIN DATA;\n\tDIMENSIONS NTAX=" + str(len(sample_names)) + " NCHAR=" + str(snp_accepted) + ";\n\tFORMAT DATATYPE=DNA" + " MISSING=N" + " GAP=- ;\nMATRIX\n"
+		output_nex.write(header_nex)
+
+	if nexusbin:
+		output_nexbin = open(outfile+".bin.nexus", "w")
+		header_nexbin = "#NEXUS\n\nBEGIN DATA;\n\tDIMENSIONS NTAX=" + str(len(sample_names)) + " NCHAR=" + str(snp_biallelic) + ";\n\tFORMAT DATATYPE=SNP" + " MISSING=?" + " GAP=- ;\nMATRIX\n"
+		output_nexbin.write(header_nexbin)
+
+
+	# Store index of outgroup in list of sample names
 	idx_outgroup = "NA"
 
 	# Write outgroup as first sequence in alignment if the name is specified
 	if outgroup in sample_names:
 		idx_outgroup = sample_names.index(outgroup)
-		with open(outfile+".tmp") as tmp_seq:
-			seqout = ""
-			for line in tmp_seq:
-				seqout += line[idx_outgroup]
 
-			# Add padding with spaces after sequence name so every nucleotide starts
-			# at the same position (only aestethic)
-			padding = (len_longest_name + 3 - len(sample_names[idx_outgroup])) * " "
+		if fasta or nexus or not phylipdisable:
+			with open(outfile+".tmp") as tmp_seq:
+				seqout = ""
 
-			# Write sample name and its corresponding sequence
-			output.write(sample_names[idx_outgroup]+padding+seqout+"\n")
+				# This is where the transposing happens
+				for line in tmp_seq:
+					seqout += line[idx_outgroup]
 
-			# Print current progress
-			print "Outgroup "+outgroup+" added to PHYLIP file."
+				# Write FASTA line
+				if fasta:
+					output_fas.write(">"+sample_names[idx_outgroup]+"\n"+seqout+"\n")
+				
+				# Pad sequences names and write PHYLIP or NEXUS lines
+				padding = (len_longest_name + 3 - len(sample_names[idx_outgroup])) * " "
+				if not phylipdisable:
+					output_phy.write(sample_names[idx_outgroup]+padding+seqout+"\n")
+				if nexus:
+					output_nex.write(sample_names[idx_outgroup]+padding+seqout+"\n")
+
+				# Print current progress
+				print "Outgroup, "+outgroup+", added to the matrix(ces)."
+
+		if nexusbin:
+			with open(outfile+".bin.tmp") as bin_tmp_seq:
+				seqout = ""
+
+				# This is where the transposing happens
+				for line in bin_tmp_seq:
+					seqout += line[idx_outgroup]
+
+				# Write line of binary SNPs to NEXUS
+				padding = (len_longest_name + 3 - len(sample_names[idx_outgroup])) * " "
+				output_nexbin.write(sample_names[idx_outgroup]+padding+seqout+"\n")
+
+				# Print current progress
+				print "Outgroup, "+outgroup+", added to the binary matrix."
+
 
 	# Write sequences of the ingroup
 	for s in range(0, len(sample_names)):
 		if s != idx_outgroup:
-			with open(outfile+".tmp") as tmp_seq:
-				seqout = ""
-				for line in tmp_seq:
-					seqout += line[s]
+			if fasta or nexus or not phylipdisable:
+				with open(outfile+".tmp") as tmp_seq:
+					seqout = ""
 
-				# Add padding with spaces after sequence name so every nucleotide starts
-				# at the same position (only aestethic)
-				padding = (len_longest_name + 3 - len(sample_names[s])) * " "
+					# This is where the transposing happens
+					for line in tmp_seq:
+						seqout += line[s]
 
-				# Write sample name and its corresponding sequence
-				output.write(sample_names[s]+padding+seqout+"\n")
+					# Write FASTA line
+					if fasta:
+						output_fas.write(">"+sample_names[s]+"\n"+seqout+"\n")
+					
+					# Pad sequences names and write PHYLIP or NEXUS lines
+					padding = (len_longest_name + 3 - len(sample_names[s])) * " "
+					if not phylipdisable:
+						output_phy.write(sample_names[s]+padding+seqout+"\n")
+					if nexus:
+						output_nex.write(sample_names[s]+padding+seqout+"\n")
 
-				# Print current progress
-				print "Sample "+str(s+1)+" of "+str(len(sample_names))+" added to PHYLIP file."
+					# Print current progress
+					print "Sample "+str(s+1)+" of "+str(len(sample_names))+", "+sample_names[s]+", added to the nucleotide matrix(ces)."
 
-	output.close()
+			if nexusbin:
+				with open(outfile+".bin.tmp") as bin_tmp_seq:
+					seqout = ""
+
+					# This is where the transposing happens
+					for line in bin_tmp_seq:
+						seqout += line[s]
+
+					# Write line of binary SNPs to NEXUS
+					padding = (len_longest_name + 3 - len(sample_names[s])) * " "
+					output_nexbin.write(sample_names[s]+padding+seqout+"\n")
+
+					# Print current progress
+					print "Sample "+str(s+1)+" of "+str(len(sample_names))+", "+sample_names[s]+", added to the binary matrix."
+
+	if not phylipdisable:
+		output_phy.close()
+	if fasta:
+		output_fas.close()
+	if nexus:
+		output_nex.write(";\nEND;\n")
+		output_nex.close()
+	if nexusbin:
+		output_nexbin.write(";\nEND;\n")
+		output_nexbin.close()
+
+	if fasta or nexus or not phylipdisable:
+		os.remove(outfile+".tmp")
+	if nexusbin:
+		os.remove(outfile+".bin.tmp")
 
 
-	# Remove temporary file
-	os.remove(outfile+".tmp")
-
-
-	print "Done!\n"
+	print "\nDone!\n"
 
 
 if __name__ == "__main__":
