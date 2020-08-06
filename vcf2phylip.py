7a94a3bcbc93b510501acba921d6702f5a65d2eb
Edgardo Ortiz <e.ortiz.v@gmail.com>
2020-08-06 07:57:19 +0200

    Update vcf2phylip.py

--- vcf2phylip.py
+++ vcf2phylip.py
@@ -62,7 +62,7 @@ def main():
         opener = open
 
     min_samples_locus = args.min_samples_locus
-    outgroup = args.outgroup.split(",").split(";")[0]
+    outgroup = args.outgroup.split(",")[0].split(";")[0]
     phylipdisable = args.phylipdisable
     fasta = args.fasta
     nexus = args.nexus
